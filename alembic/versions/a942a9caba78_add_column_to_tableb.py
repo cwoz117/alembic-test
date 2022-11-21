d2f7029c085453878bf94edff479cdd121cfcfa2
Richard Funk <richard.funk@arcurve.com>
2022-11-21 15:05:31 -0700

    Add new column to TableB

---
+++ alembic/versions/a942a9caba78_add_column_to_tableb.py
@@ -0,0 +1,25 @@
+"""add column to TableB
+
+Revision ID: a942a9caba78
+Revises: 
+Create Date: 2022-11-21 14:52:18.395146
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a942a9caba78'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('TableB', sa.Column('created', sa.DateTime))
+
+
+
+def downgrade() -> None:
+    op.drop_column('TableB', sa.Column('created'))
