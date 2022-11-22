d898115b911ddaa7e6a7b3ae16750c23b9f0b658
ali koupaei <ali_malekpourkoupaei@tcenergy.com>
2022-11-21 19:01:12 -0700

    new column issuer added

---
+++ alembic/versions/a2ed011c30cd_add_new_column_to_table_a.py
@@ -0,0 +1,25 @@
+"""add new column to table A
+
+Revision ID: a2ed011c30cd
+Revises: a942a9caba78
+Create Date: 2022-11-21 19:00:30.797607
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a2ed011c30cd'
+down_revision = 'a942a9caba78'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column(table_name='tableA',column='issuer',schema='mySchema')
+
+
+def downgrade() -> None:
+    op.drop_column(table_name='tableA',column='issuer',schema='mySchema')
+
