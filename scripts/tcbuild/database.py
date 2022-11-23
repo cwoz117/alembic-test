d45ce6d6b820e645a923bebafed3d36c65a6fac7
Christopher Wozniak <christopher.wozniak@arcurve.com>
2022-11-23 13:58:49 -0700

    ready to work

--- scripts/tcbuild/database.py
+++ scripts/tcbuild/database.py
@@ -1,11 +1,24 @@
+import boto3
+
 def check_db_exists():
+    result = False 
+
     print("does db exist?")
+    return result
 
 
 def create_database():
+    created = False
+
     print("create database")
+    return created
+
 
 
 def drop_database():
+    result = False 
     print("drop the database")
+
+    return result
+
 
