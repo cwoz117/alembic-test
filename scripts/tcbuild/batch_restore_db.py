a8ae63a8d7b05bd55db5a044d5e6b03dcd9cc146
Ali Koupaei <ali.koupaei@arcurve.com>
2022-11-23 13:31:35 -0700

    batch_restore_db() added

--- scripts/tcbuild/batch_restore_db.py
+++ scripts/tcbuild/batch_restore_db.py
@@ -9,46 +9,47 @@ database = 'tc-test-database'
 secret_arn='arn:aws:secretsmanager:us-east-1:135143936609:secret:test/redshift/password-LjFt2k'
 workgroup = 'tc-workgroup'
 
-redshift_client = boto3.client('redshift-data')
-s3_client = boto3.client('s3');
+def batch_restore_db():
+  redshift_client = boto3.client('redshift-data')
+  s3_client = boto3.client('s3');
 
-s3_response = s3_client.list_objects_v2(Bucket=source_bucket, Prefix=schema_folder)
-# if (s3_response['ResponseMetadata']['HTTPStatusCode'] != 200):
-#   error
+  s3_response = s3_client.list_objects_v2(Bucket=source_bucket, Prefix=schema_folder)
+  # if (s3_response['ResponseMetadata']['HTTPStatusCode'] != 200):
+  #   error
 
-# Get the individual table names.
-folders = [];
-for file in s3_response['Contents']:
-  folder = file['Key'].split('/')
-  folders.append(folder[0])
-unique_folders = list(set(folders))
+  # Get the individual table names.
+  folders = [];
+  for file in s3_response['Contents']:
+    folder = file['Key'].split('/')
+    folders.append(folder[0])
+  unique_folders = list(set(folders))
 
-# Generate queries to copy data to redshift db/schema.
-queries = []
-for table in unique_folders:
-  query = "copy " + dest_schema + "." + table + " from 's3://" + source_bucket + "/" + table + "/' iam_role '" + iam_role + "'"
-  queries.append(query)
+  # Generate queries to copy data to redshift db/schema.
+  queries = []
+  for table in unique_folders:
+    query = "copy " + dest_schema + "." + table + " from 's3://" + source_bucket + "/" + table + "/' iam_role '" + iam_role + "'"
+    queries.append(query)
 
-print(queries)
+  print(queries)
 
-# Execute table copy commands.
-copy_response = redshift_client.batch_execute_statement(
-  WorkgroupName=workgroup,
-  Database=database,
-  SecretArn=secret_arn,
-  Sqls=queries,
-  StatementName='copy-db-data',
-  WithEvent=False
-)
+  # Execute table copy commands.
+  copy_response = redshift_client.batch_execute_statement(
+    WorkgroupName=workgroup,
+    Database=database,
+    SecretArn=secret_arn,
+    Sqls=queries,
+    StatementName='copy-db-data',
+    WithEvent=False
+  )
 
-# checking query status
-status ='PICKED'        
-while status in ['SUBMITTED','PICKED','STARTED']:
-    state = redshift_client.describe_statement(Id=copy_response['Id'])
-    status = state['Status']        
-print(f"query status: {status}")
+  # checking query status
+  status ='PICKED'        
+  while status in ['SUBMITTED','PICKED','STARTED']:
+      state = redshift_client.describe_statement(Id=copy_response['Id'])
+      status = state['Status']        
+  print(f"query status: {status}")
 
-error = None
-if 'Error' in state:
-    error = state['Error']
-    print(error)
+  error = None
+  if 'Error' in state:
+      error = state['Error']
+      print(error)
