e85c75abd39527ccf19b9badf97b971f8111fbbc
Ali Koupaei <ali.koupaei@arcurve.com>
2022-11-23 15:50:53 -0700

    database delete/create/serach added

--- scripts/tcbuild/batch_backup_data.py
+++ scripts/tcbuild/batch_backup_data.py
@@ -2,13 +2,13 @@ import boto3
 
 import logging
 
-def run_redshift(dbname:str, query:str) -> list:
+def run_redshift(env: dict, dbname:str, query:str) -> list:
     client = boto3.client('redshift-data', region_name='us-east-1')
 
     response = client.execute_statement(
         WorkgroupName='tc-workgroup',
         Database=dbname,
-        SecretArn='arn:aws:secretsmanager:us-east-1:135143936609:secret:test/redshift/password-LjFt2k',
+        SecretArn=env['arn']['database_credentials'],
         Sql=query,
         StatementName='tc-test-deploy',
         WithEvent=False,
@@ -42,16 +42,16 @@ def run_redshift(dbname:str, query:str) -> list:
                     attrvalues += [None if 'isNull' in attr else list(attr.values())[0]]
                 values += [attrvalues]
     
-    return values
+    return values, error
 
-def run_unloads(tablelist):
+def run_unloads(env,tablelist,dbname):
     # making the unload quries
     unload_table_qrys = []
     for tablename in tablelist:
         bucket_name = f's3://tc-backup-bucket/{tablename[0]}/'
         unload_query = f"""unload ('select * from myschema.{tablename[0]}')
                            to '{bucket_name}'
-                           iam_role 'arn:aws:iam::135143936609:role/service-role/AmazonRedshift-CommandsAccessRole-20221115T143141'
+                           iam_role {env['arn']['iam_role']}
                            ALLOWOVERWRITE;
                         """
         unload_table_qrys.append(unload_query)
@@ -60,17 +60,17 @@ def run_unloads(tablelist):
         
     # running the unload queries    
     logging.warning('running unload queries...')
-    run_redshift(dbname=dbname, query=unload_table_qrys)
+    run_redshift(env=env,dbname=dbname, query=unload_table_qrys)
     logging.warning('Done.')
 
-def run_copies(tablelist):
+def run_copies(env,tablelist,dbname,):
     # making the copy quries
     copy_table_qrys = []
     for tablename in tablelist:
         bucket_name = f's3://tc-backup-bucket/{tablename[0]}/'
         copy_query = f"""copy myschema.{tablename[0]}
                            from 's3://tc-backup-bucket/{tablename[0]}/' 
-                           iam_role 'arn:aws:iam::135143936609:role/service-role/AmazonRedshift-CommandsAccessRole-20221115T143141';
+                           iam_role {env['arn']['iam_role']};
                         """
         copy_table_qrys.append(copy_query)
     
@@ -78,24 +78,26 @@ def run_copies(tablelist):
         
     # running the unload queries    
     logging.warning('running copy queries...')
-    run_redshift(dbname=dbname, query=copy_table_qrys)
+    run_redshift(env=env,dbname=dbname, query=copy_table_qrys)
     logging.warning('Done.')
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    dbname = 'dev_ali'
+#     import toml, os
+#     env = toml.load(f"{os.getcwd()}\scripts\env.toml")
+#     dbname = 'dev_ali'
 
-    # getting a list of tables for the schema
-    schemaname = 'myschema'
-    listtables_qry =f"""select t.table_name
-                    from information_schema.tables t
-                    where t.table_schema = '{schemaname}'
-                    and t.table_type = 'BASE TABLE'
-                    order by t.table_name;"""
-    tablelist = run_redshift(dbname=dbname, query=listtables_qry)
+#     # getting a list of tables for the schema
+#     schemaname = 'myschema'
+#     listtables_qry =f"""select t.table_name
+#                     from information_schema.tables t
+#                     where t.table_schema = '{schemaname}'
+#                     and t.table_type = 'BASE TABLE'
+#                     order by t.table_name;"""
+#     tablelist, _ = run_redshift(env,dbname=dbname, query=listtables_qry)
 
-    run_unloads(tablelist)
-    run_copies(tablelist)
+#     run_unloads(tablelist)
+#     run_copies(tablelist)
     
 
 
--- scripts/tcbuild/database.py
+++ scripts/tcbuild/database.py
@@ -1,24 +1,24 @@
-import boto3
+from batch_backup_data import run_redshift
+DEFAULT_DB = 'tc-test-database'
 
-def check_db_exists():
-    result = False 
+def check_db_exists(env:dict,dbname:str):
+    dbs, _= run_redshift(env=env,dbname = DEFAULT_DB, \
+            query='SELECT datname FROM pg_database;')
+   
+    return dbname in [db[0] for db in dbs]
 
-    print("does db exist?")
-    return result
+def create_database(env:dict,dbname:str):
+    _, error = run_redshift(env=env,dbname = DEFAULT_DB, \
+            query=f'create database {dbname};')
+    return not bool(error)
 
+def drop_database(env:dict,dbname:str):
+    _, error = run_redshift(env=env,dbname = DEFAULT_DB, \
+            query=f'drop database {dbname};')
+    return not bool(error)
 
-def create_database():
-    created = False
-
-    print("create database")
-    return created
-
-
-
-def drop_database():
-    result = False 
-    print("drop the database")
-
-    return result
-
-
+import toml, os
+env = toml.load(f"{os.getcwd()}\scripts\env.toml")
+# print(check_db_exists(env,'dev_alii'))
+# print(create_database(env,'test1'))
+# print(drop_database(env,'test1'))
